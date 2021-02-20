2b64d9173e3c58a32d1c73047888cd83e8cec608
yashsehgal <yashsehgal.work@gmail.com>
2021-02-20 22:47:20 +0530

    events frontend code added

--- src/js/events.js
+++ src/js/events.js
@@ -1,58 +1,99 @@
-fetch("events_list.json")
+let cardContainer = document.createElement("div");
+cardContainer.className = "card_container";
+
+/// creating DOM elements for card categories
+/// this DOM element is for Trending Events Section
+let trending_events_container = document.createElement("div");
+trending_events_container.className = "card_category";
+trending_events_container.id = "trending_events_container";
+
+/// this DOM element is for Upcoming Events Section
+let upcoming_events_container = document.createElement("div");
+upcoming_events_container.className = "card_category";
+upcoming_events_container.id = "upcoming_events_container";
+
+/// this DOM element is for new events/or the events in-general
+let new_events = document.createElement("div");
+new_events.className = "card_category";
+new_events.id = "new-events";
+
+fetch('/src/js/events_list.json')
   .then((res) => res.json())
   .then((res) => {
-    let cardContainer = document.getElementsByClassName("event_cards")[0];
+    
+
+    /// fetching data from
     for (let data in res) {
 
-      let emptyDiv = document.createElement("div");
-      emptyDiv.className = "empty_div";
-      
+      /// this is the empty <div> card for events
       let eventCard = document.createElement("div");
       eventCard.className = "event_card";
-      
-      let eventTitle = document.createElement("div");
-      
-      let heading = document.createElement("h3");
-      heading.innerText = res[data].title;
-      heading.className = "event_title";
-      
-      eventTitle.appendChild(heading);
-      
-      let startDate = document.createElement("span");
-      startDate.className = "date";
-      startDate.innerHTML = `<b>Starts:</b> ${res[data].start}`;
-      
-      let endDate = document.createElement("span");
-      endDate.className = "date";
-      endDate.innerHTML = `<b>Ends:</b> ${res[data].end}`;
-      
-      let eventDetails = document.createElement("div");
-      eventDetails.className = "event_details";
-      
-      let eventLink = document.createElement("a");
-      
-      Object.assign(eventLink, {
-        href: `https://${res[data].website}`,
-        target: "_blank",
-        rel: "noopener noreferrer",
-        className: "btn btn-primary link",
-      });
-      
-      let link = document.createElement("h6");
-      link.innerText = "More Info";
-      
-      eventLink.appendChild(link);
-      
-      let organisation = document.createElement("p");
-      organisation.innerHTML = `<b>Organisation: ${res[data].organisation}`;
-      
-      let loc = document.createElement("h5");
-      loc.className = "location";
-      loc.innerText = res[data].location;
-      
-      eventDetails.append(startDate, endDate, organisation, loc, eventLink);
-      eventCard.append(eventTitle, eventDetails);
-      emptyDiv.appendChild(eventCard);
-      cardContainer.appendChild(emptyDiv);
+
+      let event_title = document.createElement("h3");
+      event_title.id = "event-title";
+      event_title.innerHTML = res[data].title;
+
+      let event_website = document.createElement("a");
+      event_website.id = "event-website";
+      event_website.innerHTML = `<a href="${res[data].website}" style="
+          text-decoration: none;
+
+      ">${res[data].website}</a>`;
+
+      let event_duration = document.createElement("p");
+      event_duration.id = "event-duration";
+      event_duration.innerHTML = `<b>${res[data].start}</b> to <b>${res[data].end}</b>`;
+
+      let event_organization = document.createElement("p");
+      event_organization.id = "event-organization";
+      event_organization.innerHTML = res[data].organization;
+
+      // let tag = res[data].tag;
+
+      let event_mode = document.createElement("p");
+      event_mode.id = "event-mode";
+      event_mode.innerHTML = res[data].location;
+
+      let learnMoreButton = document.createElement("a");
+      learnMoreButton.className = "button";
+      learnMoreButton.id = "learn-more-button";
+      learnMoreButton.innerHTML =  `Learn More`;
+
+      /// setting up data inside the card
+      eventCard.append(event_title);
+      eventCard.append(event_website);
+      eventCard.append(event_duration);
+      eventCard.append(event_organization);
+      eventCard.append(event_mode);
+      eventCard.append(learnMoreButton);
+
+      if (res[data].tag.toLowerCase() == "trending") {
+        /// the data card will be later appended inside the
+        /// trending section
+        trending_events_container.append(eventCard);
+      } else if (res[data].tag.toLowerCase() == "upcoming") {
+        /// the data card will be later appended inside the
+        /// upcoming section
+        upcoming_events_container.append(eventCard);
+      } else {
+        new_events.append(eventCard);
+      }
+
     }
+
+    // document.getElementById("event-card").ondblclick((MouseEvent) => function(res) {
+    //   document.getElementById("root").innerHTML = "clicked!!";
+    // });
+
+    cardContainer.append(new_events);
+    cardContainer.append(trending_events_container);
+    cardContainer.append(upcoming_events_container);
+
+    // document.getElementById("new-events").innerHTML('<h2>New Events</h2>');
+    document.getElementById("new-events").appendChild(new_events);
+    document.getElementById("trending-events").appendChild(trending_events_container);
+    document.getElementById("upcoming-events").appendChild(upcoming_events_container);
+
   });
+
+document.getElementById("root").appendChild(cardContainer);
